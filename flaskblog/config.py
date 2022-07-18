9a576d3a9eccee9a2d48f070074425b205a8d15c
Ravishankar Naik <rgnaik96@gmail.com>
2022-07-18 20:43:09 +0530

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -12,5 +12,5 @@ class Config():
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'rgnaik96@gmail.com'
+    MAIL_USERNAME = '...@gmail.com'
     MAIL_PASSWORD = '<password>'
